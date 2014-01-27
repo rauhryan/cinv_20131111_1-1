1dd070559258728687bba0315de094177d1f981d
Barrett Sonntag <barrett.sonntag@prometheanworld.com>
2014-01-27 07:33:28 -0800

    added movie library object

--- app/assets/javascripts/movies/movie_library.js
+++ app/assets/javascripts/movies/movie_library.js
@@ -1,4 +1,21 @@
 (function(targetNamespace){
 
-})(this);
+  function MovieLibrary(movies){
+    this.movies = movies;
+  }
+  
+  var proto = MovieLibrary.prototype;
 
+  proto.allPixarMovies = function() {
+  	var pixarMovies = [];
+  	for (var i = 0; i < this.movies.length; i++) {
+  		if(this.movies[i].studio === STUDIOS.PIXAR) {
+  			pixarMovies.push(this.movies[i]);
+  		}
+  	};
+  	return pixarMovies;
+  };
+
+  targetNamespace.MovieLibrary = MovieLibrary;
+
+})(this);
